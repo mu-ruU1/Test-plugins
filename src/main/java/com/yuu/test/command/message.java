4bff2f8038787d93c37ebb12c6af4e78e888b00d
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-02-14 17:26:21 +0900

    引数による返信の変更完成

--- src/main/java/com/yuu/test/command/message.java
+++ src/main/java/com/yuu/test/command/message.java
@@ -1,7 +1,6 @@
 package com.yuu.test.command;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,11 +10,22 @@ public class message implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        //コマンドを実行した人に送信
-        sender.sendMessage("[" + ChatColor.GREEN + "メッセージ01" + ChatColor.WHITE + "]");
+        if (args[0].equalsIgnoreCase("1")){
+            sender.sendMessage("サブコマンド1");
+        }else if (args[0].equalsIgnoreCase("2")){
+            sender.sendMessage("サブコマンド2");
+        }else if (args[0].equalsIgnoreCase("3")){
+            sender.sendMessage("サブコマンド3");
+        }else{
+            sender.sendMessage("サブコマンドは1,2,3しかありません");
+        }
 
-        //サーバー全体に送信
-        Bukkit.broadcastMessage("全員に送信");
+
+//        //コマンドを実行した人に送信
+//        sender.sendMessage("[" + ChatColor.GREEN + "メッセージ01" + ChatColor.WHITE + "]");
+//
+//        //サーバー全体に送信
+//        Bukkit.broadcastMessage("全員に送信");
         return true;
     }
 }
